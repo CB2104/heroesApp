a58776b31f346a846422d55f5c05c697e5a75c93
CB2104 <141430425+CB2104@users.noreply.github.com>
2026-01-18 11:02:21 -0300

    FirstPull

---
+++ src/heroes/pages/search/SearchPage.tsx
@@ -0,0 +1,50 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { HeroStats } from "@/heroes/components/HeroStats";
+import { SearchControls } from "./ui/SearchControls";
+import { CustomBreadcrumbs } from "@/components/custom/CustomBreadcrumbs";
+import { HeroGrid } from "@/heroes/components/HeroGrid";
+import { useSearchParams } from "react-router";
+import { searchHeroesAction } from "@/heroes/actions/search-hero.action";
+
+export const SearchPage = () => {
+  const [searchParams] = useSearchParams();
+
+  const name = searchParams.get("name") ?? undefined;
+  const strength = searchParams.get("strength") ?? undefined;
+
+  const { data: heroes = [] } = useQuery({
+    queryKey: ["search", { name, strength }],
+    queryFn: () => searchHeroesAction({ name,strength }),
+    staleTime: 1000 * 60 * 5, //5 min
+  });
+
+  return (
+    <>
+      <CustomJumbotron
+        title="Busqueda de SuperHeroes"
+        description="Descubre, explora y administra super heroes y villanos"
+      />
+
+      <CustomBreadcrumbs
+        currentPage="Buscador de Heroes"
+        // breadcrumb={[
+        //   { label: "Home1", to: "/" },
+        //   { label: "Home2", to: "/" },
+        //   { label: "Home3", to: "/" },
+        // ]}
+      />
+
+      {/* Stats dashboard */}
+      <HeroStats />
+
+      {/* Filter and search */}
+      <SearchControls />
+
+      <HeroGrid heroes={heroes} />
+    </>
+  );
+};
+
+export default SearchPage;
